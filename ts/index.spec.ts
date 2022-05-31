a67af567c21e503ba5bf52e00cfa80ffd8dba118
Andreas Lundqvist <momentiris@gmail.com>
2022-05-31 22:13:28 +0200

    feat(ts): add solution 7 and 8

--- ts/index.spec.ts
+++ ts/index.spec.ts
@@ -1,22 +1,31 @@
-import { last, butLast, at, length, reverse, isPalindrome } from '.'
+import {
+  last,
+  butLast,
+  at,
+  length,
+  reverse,
+  isPalindrome,
+  flatten,
+  compress,
+} from '.';
 
 describe('01 - Find the last element of a list.', () => {
   it('works', () => {
-    const cases = [[], [0], [0, 1]]
-    const result = cases.flatMap(last)
+    const cases = [[], [0], [0, 1]];
+    const result = cases.flatMap(last);
 
-    expect(result).toEqual([undefined, 0, 1])
-  })
-})
+    expect(result).toEqual([undefined, 0, 1]);
+  });
+});
 
 describe('02 - Find the last but one element of a list.', () => {
   it('works', () => {
-    const cases = [[], [0], [0, 1], [0, 1, 2]]
-    const result = cases.flatMap(butLast)
+    const cases = [[], [0], [0, 1], [0, 1, 2]];
+    const result = cases.flatMap(butLast);
 
-    expect(result).toEqual([undefined, undefined, 0, 1])
-  })
-})
+    expect(result).toEqual([undefined, undefined, 0, 1]);
+  });
+});
 
 describe('03 - Find the K:th element of a list.', () => {
   it('works', () => {
@@ -25,13 +34,13 @@ describe('03 - Find the K:th element of a list.', () => {
       [[0], 2],
       [[0, 1], 1],
       [[0, 1, 2], 3],
-    ]
+    ];
 
-    const result = cases.flatMap(([xs, k]) => at(xs, k))
+    const result = cases.flatMap(([xs, k]) => at(xs, k));
 
-    expect(result).toEqual([undefined, undefined, 0, 2])
-  })
-})
+    expect(result).toEqual([undefined, undefined, 0, 2]);
+  });
+});
 
 describe('04 - Find the number of elements of a list.', () => {
   it('works', () => {
@@ -40,28 +49,71 @@ describe('04 - Find the number of elements of a list.', () => {
       [undefined],
       [0],
       [0, 1, 2],
-    ]
+    ];
 
     const assert = (xs: Array<number | undefined>) =>
-      expect(length(xs)).toEqual(xs.length)
-    cases.forEach(assert)
-  })
-})
+      expect(length(xs)).toEqual(xs.length);
+    cases.forEach(assert);
+  });
+});
 
 describe('05 - Reverse a list.', () => {
   it('works', () => {
-    const cases: Array<Array<number>> = [[], [0], [0, 1], [0, 1, 2]]
+    const cases: Array<Array<number>> = [[], [0], [0, 1], [0, 1, 2]];
 
     const assert = (xs: Array<number>) =>
-      expect(reverse(xs)).toEqual(xs.reverse())
-    cases.forEach(assert)
-  })
-})
+      expect(reverse(xs)).toEqual(xs.reverse());
+    cases.forEach(assert);
+  });
+});
 
 describe('06 - Find out whether a list is a palindrome.', () => {
   it('works', () => {
-    const cases = [[0, 1, 2], 'sirapiparis', [0, 1, 0]]
-    const result = cases.map((c) => isPalindrome(c))
-    expect(result).toEqual([false, true, true])
-  })
-})
+    const cases = [[0, 1, 2], 'sirapiparis', [0, 1, 0]];
+    const result = cases.map((c) => isPalindrome(c));
+    expect(result).toEqual([false, true, true]);
+  });
+});
+
+describe('06 - Find out whether a list is a palindrome.', () => {
+  it('works', () => {
+    const cases = [[0, 1, 2], 'sirapiparis', [0, 1, 0]];
+    const result = cases.map((c) => isPalindrome(c));
+    expect(result).toEqual([false, true, true]);
+  });
+});
+
+describe('07 - Flatten a nested list structure.', () => {
+  it('works', () => {
+    const a = [0, 1, 2];
+    const b = [3, 4, 5];
+    const c = [a, b];
+
+    expect(flatten(c)).toEqual(c.flat());
+  });
+});
+
+describe('08 - Eliminate consecutive duplicates of list elements.', () => {
+  it('works', () => {
+    const input = [
+      'a',
+      'a',
+      'a',
+      'a',
+      'b',
+      'c',
+      'c',
+      'a',
+      'a',
+      'd',
+      'e',
+      'e',
+      'e',
+      'e',
+    ];
+
+    const expected = ['a', 'b', 'c', 'a', 'd', 'e'];
+
+    expect(compress(input)).toEqual(expected);
+  });
+});
--- ts/index.ts
+++ ts/index.ts
@@ -1,37 +1,49 @@
 export function last<T>([x, ...xs]: Array<T>): T | undefined {
-  if (xs.length === 0) return x
+  if (xs.length === 0) return x;
 
-  return last(xs)
+  return last(xs);
 }
 
 export function butLast<T>([x, ...xs]: Array<T>): T | undefined {
-  if (x === undefined) return undefined
-  if (xs.length === 1) return x
+  if (x === undefined) return undefined;
+  if (xs.length === 1) return x;
 
-  return butLast(xs)
+  return butLast(xs);
 }
 
 export function at<T>([x, ...xs]: Array<T>, k: number): T | undefined {
-  if (x !== undefined && k === 1) return x
-  if (xs && k > 1) return at(xs, k - 1)
+  if (x !== undefined && k === 1) return x;
+  if (xs && k > 1) return at(xs, k - 1);
 
-  return undefined
+  return undefined;
 }
 
 export function length<T>(xs: Array<T>): number {
-  return xs.reduce((acc) => acc + 1, 0)
+  return xs.reduce((acc) => acc + 1, 0);
 }
 
 export function reverse<T>(xs: Array<T>): Array<T> {
   function r<T>([x, ...xs]: Array<T>, acc: Array<T>): Array<T> {
-    if (x === undefined) return acc
-    return r(xs, [x, ...acc])
+    if (x === undefined) return acc;
+    return r(xs, [x, ...acc]);
   }
 
-  return r(xs, [])
+  return r(xs, []);
 }
 
 export function isPalindrome(xs: string | Array<string | number>): boolean {
-  const norm = [...xs]
-  return norm.join('') === norm.reverse().join('')
+  const norm = [...xs];
+  return norm.join('') === norm.reverse().join('');
+}
+
+export function flatten<T>(xs: Array<T>): Array<T> {
+  return xs.reduce(
+    (acc: Array<T>, cur: T) =>
+      acc.concat(Array.isArray(cur) ? flatten(cur) : cur),
+    []
+  );
+}
+
+export function compress<T extends string>(xs: Array<T>): Array<T> {
+  return xs.filter((x, idx, xs) => idx === 0 || x !== xs[idx - 1]);
 }
